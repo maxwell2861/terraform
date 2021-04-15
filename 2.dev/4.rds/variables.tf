c410f06d273e5623350757ea871698b765a46c91
maxwell <maxwell@lezhin.com>
2021-04-15 14:10:28 +0900

    modify-dev rds db engine version

--- 2.dev/4.rds/variables.tf
+++ 2.dev/4.rds/variables.tf
@@ -18,24 +18,24 @@ locals {
   
   network_info = { 
     db_subnet_group    = data.terraform_remote_state.network.outputs.dev_db_subnet_group
-    common_sg          = data.terraform_remote_state.network.outputs.dev_common_sg
     db_sg              = data.terraform_remote_state.network.outputs.dev_rds_sg
-    az_zone            = [data.aws_availability_zones.az.names[0],data.aws_availability_zones.az.names[2]]
+    common_sg          = data.terraform_remote_state.network.outputs.dev_common_sg
+    az_zone            = [d.aws_availability_zones.az.names[0]ata,data.aws_availability_zones.az.names[2]]
   }
 
   rds_info = {
-        dev-test-rds = {
-              name            = "rds"
-              type            = "db.t3.smaill"
-              engine          = "5.7.mysql_aurora.2.09.0"
+        dev-kdr-db = {
+              name            = "db"
+              type            = "db.t3.small"
+              engine          = "5.7"
               port            = 3306
-              username        = local.common_info.db_creds.CLUSTER_USERNAME
-              password        = local.common_info.db_creds.CLUSTER_PASSWORD
+              username        = local.common_info.db_creds.CLUSTER_MASTER_USERNAME
+              password        = local.common_info.db_creds.CLUSTER_MASTER_PASSWORD
               subnet_group    = local.network_info.db_subnet_group
-              sg              = local.network_info.db_sg
+              sg              = [local.network_info.db_sg,local.network_info.common_sg]
               az              = local.network_info.az_zone
               #Paremeter Groups
-                family_version  = "aurora-mysql5.7"
+                family_version  = "mysql5.7"
                 parameters = {
                     binlog-format =  {
                       #disable Cluster Mode
